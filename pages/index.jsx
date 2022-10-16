5be15464df1302afc5b64f3a518d0360fddf3755
Guilherme Cabral <gui.cabral7641@gmail.com>
2022-10-16 02:21:22 -0300

    homepage done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>Costs</h1>
-        <h1>stsoC</h1>
+    <section className={styles.home_container}>
+      <h1>
+        Bem vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar seus projetos agora mesmo!</p>
+      <Link href="/new_project">
+        <a>Criar projetos</a>
+      </Link>
+      <div className={styles.imagem}>
+        <Image src="/img/savings.svg" width="350" height="350" alt="cofre" />
       </div>
-    </>
+    </section>
   );
 }
