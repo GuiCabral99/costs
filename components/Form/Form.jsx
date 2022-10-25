c6da27312f28b2822ec946fbe221cc7f25b7e6cf
Guilherme Cabral <gui.cabral7641@gmail.com>
2022-10-25 09:01:20 -0300

    project creation done

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 import Input from "./Input";
 import Select from "./Select";
@@ -7,38 +8,69 @@ import Btn from "./Btn";
 import styles from "../../styles/Form/Form.module.css";
 
 export default function Form() {
-  const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState({}); // useState com as informações dos projetos
+  const [category, setCategory] = useState([]);
+  const router = useRouter();
 
-  useEffect(() => {
-    fetch("http://localhost:3001/categories", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+  // Criando projeto no backend em formato json
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+    fetch("http://localhost:3001/projects", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(project),
     })
-      .then((res) => res.json())
-      .then((data) => setCategories(data))
+      .then((res) => res.json)
+      .then((data) => router.push("/projects")) //redirect
       .catch((err) => console.log(err));
-  }, []);
+  }
+
+  // Mudando o valor do useState project atraves dos inputs project e budget
+  const inputChange = (e) => {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  };
+
+  // Adicionando select/options no backend
+  const selectCategory = (e) => {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  };
+
+  // submit
+  const submit = (e) => {
+    e.preventDefault();
+    createPost(project);
+  };
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto:"
         placeholder="Insira o nome do projeto"
         name="project"
+        onChange={inputChange}
+        value={project.name}
       />
       <Input
         text="Orçamento do projeto:"
         placeholder="Insira o orçamento total"
         type="number"
         name="budget"
+        onChange={inputChange}
+        value={project.budget}
       />
       <Select
         name="category"
         labelText="Selecione a categoria: "
-        options={categories}
+        onchange={selectCategory}
+        value={project.category ? project.category.id : ""}
       />
       <Btn text="Criar projeto" />
     </form>
--- components/Form/Select.jsx
+++ components/Form/Select.jsx
@@ -1,14 +1,26 @@
+import { useState, useEffect } from "react";
+
 import styles from "../../styles/Form/Select.module.css";
 
-export default function Select({ name, value, text, labelText, options }) {
+export default function Select({ name, labelText, onchange, value }) {
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.select_container}>
       <label htmlFor={name}>{labelText}</label>
-      <select name={name} id={name}>
-        <option disabled selected>
-          Selecione a categoria
-        </option>
-
+      <select name={name} id={name} onChange={onchange} value={value || ""}>
         {options.map((option) => (
           <option value={option.id} key={option.id}>
             {option.name}
